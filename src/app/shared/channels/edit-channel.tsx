353eedfdbc3b18bb15f5f8368ea5ca3991c3a29b
aitezazimtiaz <aitezaz.imtiaz@chirptech.net>
2024-03-13 21:25:32 +0500

    fix: event image update issue fixed

--- src/app/shared/channels/edit-channel.tsx
+++ src/app/shared/channels/edit-channel.tsx
@@ -6,6 +6,8 @@ import useGetChannel from "@/api/Channels/useGetChannel";
 import toast from "react-hot-toast";
 import S3UploadHandlerMutationFn from "@/api/S3UploadHandlerMutationFn";
 import { type FileSchema } from "@/lib/zod-schemas/attachment";
+import { useRouter } from "next/navigation";
+import { routes } from "@/config/routes";
 
 type Props = {
   id: string;
@@ -13,10 +15,11 @@ type Props = {
 };
 
 const EditChannel = ({ id, accessToken }: Props) => {
-  const { data, status } = useGetChannel({
+  const { data, status, refetch } = useGetChannel({
     id,
   });
   const { mutateAsync } = useUpdateChannelMutation();
+  const router = useRouter();
 
   if (status === "pending") {
     return "Loading...";
@@ -24,6 +27,24 @@ const EditChannel = ({ id, accessToken }: Props) => {
   if (status === "error") {
     return "Error";
   }
+  console.log("Initial Values", {
+    title: data?.getChannelById.title,
+    about: data?.getChannelById.about,
+    rules: data?.getChannelById.rules,
+    media: {
+      image: data?.getChannelById.image,
+      bannerImage: data?.getChannelById.backgroundImage,
+    },
+    moderator: {
+      id: data?.getChannelById.moderator.id,
+      firstName: data?.getChannelById.moderator.firstName,
+      lastName: data?.getChannelById.moderator.lastName,
+      email: data?.getChannelById.moderator.email,
+    },
+    visibility: data?.getChannelById.status,
+    price: (data?.getChannelById.price ?? 0) + "",
+  });
+
   return (
     <ChannelForm
       id={id}
@@ -61,6 +82,15 @@ const EditChannel = ({ id, accessToken }: Props) => {
               ? await S3UploadHandlerMutationFn(filesToBeUploaded, accessToken)
               : [undefined, undefined];
 
+          console.log("Sending values:", {
+            id: id,
+            title: data.title,
+            about: data.about,
+            rules: data.rules,
+            backgroundImage: bannerImage?.uploadedURL,
+            image: profileImage?.uploadedURL,
+          });
+
           await mutateAsync({
             input: {
               id: id,
@@ -72,6 +102,9 @@ const EditChannel = ({ id, accessToken }: Props) => {
             },
           });
           toast.success("Channel updated successfully");
+          await refetch();
+
+          // router.push(routes.channels.list);
         } catch (e) {
           toast.error("Error updating channel");
         }
